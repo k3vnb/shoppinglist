677d11e5c449a502666b6fe31a770d1405021d8a
Ginger Lee Kretschmer and Kevin Boyle <kevin@>
2017-10-23 16:32:58 -0700

    take user form inputs and return a sorted uppercase list

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,30 @@
 $(document).ready(function() {
-  $("button").click(function() {
-    var flavors = ["vanilla", "mint chocolate chip", "caramel"];
-    flavors.forEach(function(flavor) {
-    $("ul").prepend("<li>" + flavor + "</li>");
-  });
+  $("form").submit(function(event) {
+    var items = ["item1", "item2", "item3", "item4"];
+    var myList = [];
 
+    items.forEach(function(item) {
+      var userInput = $("input#" + item).val();
+      myList.push(userInput);
+    });
+
+    var myListUpper = myList.map(function(item) {
+      return item.toUpperCase();
+    });
+
+    var myListUpperSort = myListUpper.sort();
+    myListUpperSort.forEach(function(li) {
+      $(".flavs").append("<li>" + li + "</li>");
+    })
+
+
+  event.preventDefault();
   });
 });
+
+
+
+
+//   flavors.forEach(function(flavor) {
+//   $("ul").prepend("<li>" + flavor + "</li>");
+// });
